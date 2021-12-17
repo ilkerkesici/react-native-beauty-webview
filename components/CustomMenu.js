1453e9e6f16881164cadf4cf327a50c1413fe4be
esadkrs <esadkiris@gmail.com>
2021-12-17 12:12:03 -0500

    Issue #11: fix – use Clipboard from deprecated react native package

--- components/CustomMenu.js
+++ components/CustomMenu.js
@@ -2,9 +2,9 @@ import React, {useState} from 'react';
 import Menu from './Menu';
 import MenuItem from './MenuItem';
 import MenuDivider from './MenuDivider';
-import { StyleSheet, Image, TouchableOpacity, Linking, View } from 'react-native';
+import { StyleSheet, Image, TouchableOpacity, Linking, View, Clipboard } from 'react-native';
 import { images } from '../res';
-import Clipboard from "@react-native-community/clipboard";
+
 
 export const CutomMenu = ({contentType, openBrowserTitle, copyLinkTitle, url, extraMenuItems}) => {
     const [menu, setMenu] = useState(null);
